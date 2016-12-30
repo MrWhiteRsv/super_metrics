e71dcc7a0627b1d3439ad2195774baabab772f4f
Mr White <whitersrvdg@gmail.com>
2016-12-30 12:58:22 -0800

    Shift player to the cloudmqtt broker instead of the linode one.

--- utils/player.py
+++ utils/player.py
@@ -7,12 +7,12 @@ import json
 import paho.mqtt.publish as publish
 
 def pub(topic, payload):
-  hostname = "li1109-31.members.linode.com"
-  publish.single(topic, payload, hostname=hostname) 
-
+  #hostname = "li1109-31.members.linode.com"
+  hostname = "m13.cloudmqtt.com"
+  auth = {'username':"oujibpyy", 'password':"-mKBDKwYQ1CC"}
+  publish.single(topic, payload, hostname=hostname, auth=auth, port=11714) 
    
-def publish_log(file_name):  
-  #log_file = open('path_1_stragiht_line.dat', 'r')
+def publish_log(file_name):
   log_file = open(file_name)
   record_start_time = None
   play_start_time = int(time.time())
@@ -29,39 +29,30 @@ def publish_log(file_name):
       continue
     if (topic == 'cart/cartId/hall_reading'):
       continue
-    #print '\n____________________________________'
     line_time = parsed_line['start_time']
     if not record_start_time:
       record_start_time = line_time
     time_since_record_start = parsed_line['start_time'] - record_start_time
-    """while (True):
-      time_since_play_start = int(time.time()) - play_start_time
-      print ('time_since_record_start', time_since_record_start)
-      print ('time_since_play_start', time_since_play_start)
-      wait = time_since_record_start >= time_since_play_start
-      if not wait:
-        break 
-      time.sleep(1)"""
     parsed_line.pop('topic', 0)
     pub(topic, json.dumps(parsed_line))
-    print (topic, parsed_line)
-    
+    print(topic, parsed_line)
   log_file.close()
   
 def main(argv):
   log_file_name = 'tiny.dat'
   try:
-    opts, args = getopt.getopt(argv,"hi:",["infile="])
+    opts, args = getopt.getopt(argv, "hi:", ["infile="])
   except getopt.GetoptError:
     print 'python player.py -i <log_file>'
     sys.exit(2)
+  #hostname = "li1109-31.members.linode.com"
   for opt, arg in opts:
     if opt == '-h':
       print 'python player.py -i <log_file>'
       sys.exit()
-    elif opt in ("-i", "--infile"):
+    if opt in ("-i", "--infile"):
       log_file_name = arg
-    publish_log(log_file_name)
+  publish_log(file_name = log_file_name)
   
   
 if __name__ == '__main__':
